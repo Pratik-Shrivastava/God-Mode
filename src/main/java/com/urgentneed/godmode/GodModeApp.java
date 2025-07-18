88ebf06787b41253df9c1f63bfc7df697145df0c
Pratik-Shrivastava <pratikkumar441@gmail.com>
2025-07-18 22:26:53 +0530

    feat: initialize God-Mode app

---
+++ src/main/java/com/urgentneed/godmode/GodModeApp.java
@@ -0,0 +1,13 @@
+package com.urgentneed.godmode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GodModeApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GodModeApp.class, args);
+	}
+}
