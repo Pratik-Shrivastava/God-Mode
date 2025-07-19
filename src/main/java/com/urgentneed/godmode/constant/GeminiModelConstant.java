2ff466e87738be17c8e956792e6e2d33382f689c
Pratik-Shrivastava <pratikkumar441@gmail.com>
2025-07-19 22:39:19 +0530

    feat: add GeminiModelConstant file to add model names as constants

---
+++ src/main/java/com/urgentneed/godmode/constant/GeminiModelConstant.java
@@ -0,0 +1,9 @@
+package com.urgentneed.godmode.constant;
+
+public class GeminiModelConstant {
+    public static final String GEMINI_2_5_FLASH = "gemini-2.5-flash";
+
+    public static final String GEMINI_IMAGE_GENERATION =
+                                    "gemini-2.0-flash-exp-image-generation";
+
+}
